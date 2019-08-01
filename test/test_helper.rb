3714a931831d40b24d72f4bf1dfc52d88a13d242
Ethan <ethan@unth.net>
2019-08-01 01:50:07 -0700

    testing: simplecov, byebug, minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,19 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "arms"
+proc { |p| $:.unshift(p) unless $:.any? { |lp| File.expand_path(lp) == p } }.call(File.expand_path('../lib', File.dirname(__FILE__)))
 
-require "minitest/autorun"
+require 'simplecov'
+require 'byebug'
+
+# NO EXPECTATIONS
+ENV["MT_NO_EXPECTATIONS"] = ''
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class ARMSSpec < Minitest::Spec
+end
+
+# register this to be the base class for specs instead of Minitest::Spec
+Minitest::Spec.register_spec_type(//, ARMSSpec)
+
+require 'arms'
