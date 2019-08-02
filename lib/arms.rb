05bb60a32547e250b7e79886476e0aa4cffd99c6
Ethan <ethan@unth.net>
2019-08-01 21:17:42 -0700

    ARMS.register_coder_shortcut

--- lib/arms.rb
+++ lib/arms.rb
@@ -3,4 +3,14 @@ require "arms/version"
 module ARMS
   class Error < StandardError
   end
+
+  @coder_shortcuts = {}
+
+  class << self
+    def register_coder_shortcut(key, &coderproc)
+      raise(ArgumentError, "already registered shortcut: #{key}") if @coder_shortcuts.key?(key)
+      @coder_shortcuts[key] = coderproc
+      nil
+    end
+  end
 end
